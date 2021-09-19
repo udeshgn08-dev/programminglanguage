9a160f82dec762891cdd8ea8ef79b7529880629d
UdeshGLeksh <85166833+udeshgn08-dev@users.noreply.github.com>
2021-09-19 21:26:01 +0530

    Add files via upload

---
+++ practise/NumericArgs.java
@@ -0,0 +1,35 @@
+package practise;
+
+public class NumericArgs {
+
+	public static void main(String[] args) {
+		int argOne;
+		double argTwo;
+		float argThree;
+		if (args.length > 0) {
+			try {
+				argOne = Integer.parseInt(args[0]);
+				System.out.print("Integeral " + argOne + ",");
+			} catch (NumberFormatException ne) {
+				System.err.println("Whole numbers must be integeral");
+				System.exit(1);
+			}
+			try {
+				argTwo = Double.parseDouble(args[1]);
+				System.out.print("Double " + argTwo + ",");
+			} catch (NumberFormatException ae) {
+				System.err.println("Precision point must be double");
+				System.exit(1);
+			}
+			try {
+				argThree = Float.parseFloat(args[2]);
+				System.out.print("Float " + argThree);
+			} catch (NumberFormatException ae) {
+				System.err.println("Fractional part must be float");
+				System.exit(1);
+			}
+		}
+
+	}
+
+}
