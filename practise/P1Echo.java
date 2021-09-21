2fe9f78eaeb61bd5a24fc31edf1fd12c38ae9a93
UdeshGLeksh <85166833+udeshgn08-dev@users.noreply.github.com>
2021-09-21 21:30:00 +0530

    Add files via upload

---
+++ practise/P1Echo.java
@@ -0,0 +1,15 @@
+/*
+ * When an application is launched, the runtime system passes the arguments 
+ * to the application's main method via an array of strings.  
+ */
+package practise;
+
+public class P1Echo {
+
+	public static void main(String[] args) {
+		for (String var : args) {
+			System.out.println(var);
+		}
+
+	}
+}
---
+++ practise/P3Helper.java
@@ -0,0 +1,24 @@
+/**
+ * Java is a statically typed language meaning all variables must be type declared before using it. 
+ * Numeric Primitive types are int, long, double, float, byte, short
+ */
+package practise;
+public class P3Helper {
+
+	public static void main(String[] args) {
+		
+          byte byt = 127; 
+          short sht = 32000; 
+          int in_t = 10000; 
+          long lng = 1000000L; 
+          float flt = 5689.5F; 
+          double dbl = 9054.1D; 
+          System.out.println("Byte: "+byt);
+          System.out.println("Short: "+sht);
+          System.out.println("Integer: "+in_t);
+          System.out.println("Long: "+lng);
+          System.out.println("Float: "+flt);
+          System.out.println("Double: "+dbl);
+	}
+
+}
