ce5289aba9de4e1b736f50f2712113eb5665af39
UdeshGLeksh <85166833+udeshgn08-dev@users.noreply.github.com>
2021-09-30 20:52:01 +0530

    P0 > Program structure

---
+++ harvard/escapesequence/P0Syntax.java
@@ -0,0 +1,34 @@
+/**
+ * This is the overview of how java programming language looks like 
+ */
+package set00;
+
+// Here, import classes if that's necessary for the program development 
+
+public class P0Syntax {
+	/**
+	 * The main method always begins with the same header.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// In java, all programs consist of one of more classes
+		// Think of a class as a container for the program
+		// Code inside the method is enclosed in curly braces
+		// Statement typically ends with semicolon
+		// Statements are executed sequentially
+		// Identifiers are naming combination of java programs
+		// Identifiers cannot be the same as a keyword
+		// Java is case-sensitive meaning System.out.print is not the same as system.out.Print
+		// Literal specified explicitly in the program 
+		// String literal enclosed in "Computed Result" and char literals in 'A' 
+		// Constructor name must be the same as Class name 
+        // Create an instances of class by creating object for the class. 
+		// { braces are very important part of program flow such as if,for,while,do-while and switch } 
+		// However, squared[] are used to index access elements in arrays,strings and angular<> frequently used in data structures   
+		/*
+		 * @see oracle doc
+		 */
+	}
+
+} // End of the line
