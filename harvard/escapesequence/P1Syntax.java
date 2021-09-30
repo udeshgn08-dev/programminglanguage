662e21d7ad1b7231247f887e21b136fba8d217d2
UdeshGLeksh <85166833+udeshgn08-dev@users.noreply.github.com>
2021-09-30 19:30:27 +0530

    P1 > Insert a new tab at the specific point

---
+++ harvard/escapesequence/P1Syntax.java
@@ -0,0 +1,15 @@
+package set00;
+
+public class P1Syntax {
+
+	public static void main(String[] args) { 
+		// General syntax 
+		int firstNumber = 5;
+		int secondNumber = 5;
+		int product = firstNumber * secondNumber; 
+		System.out.println("\tProduct of 5*5=" + product);
+		System.out.println("\tProduct of 5*5=" + (firstNumber * secondNumber));
+		System.out.println("\tProduct of 5*5=" + 5 * 5);
+
+	}
+}
