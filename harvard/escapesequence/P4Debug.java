1af2b2bac43cf932f9347cc73ce16d596657981e
UdeshGLeksh <85166833+udeshgn08-dev@users.noreply.github.com>
2021-10-01 08:49:47 +0530

    P4 > Fix bugs

---
+++ harvard/escapesequence/P4Debug.java
@@ -0,0 +1,17 @@
+/*
+ * Launch debug 
+ * Set a breakpoint 
+ * Inspect the program
+ */
+package set00;
+
+public class P4Debug {
+	public static void methodB() {
+		System.out.println("I need some Java");
+		}
+		public static void main(String[] args) {
+		methodB();
+		}
+		}
+
+
