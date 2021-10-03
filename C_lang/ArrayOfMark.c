ffe442690a7b5a3083ba500ac2072e1d0d4635c3
UdeshGLeksh <85166833+udeshgn08-dev@users.noreply.github.com>
2021-10-03 18:41:27 +0530

    Add files via upload

---
+++ C_lang/ArrayOfMark.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+
+int main(){
+    int MarkSet[10] = {98,86,71,67,66,60,56,53,45,37}; 
+    double grade[5]; 
+    grade[0] = 7.5;
+    grade[1] = 6.1; 
+    grade[2] = 6.1;
+    grade[3] = 6.0;
+    grade[4] = 5.5; 
+    for(int count=0; count < 10; count++){
+    printf("#Mark:%d\n",MarkSet[count]); 
+    }
+        for(int gradeCount = 0; gradeCount < 5; gradeCount++ ){ 
+        printf("#Grade:%f\n",grade[gradeCount]);     
+    }
+    return 0; 
+}
