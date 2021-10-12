19357a231338469d5db1b1de9b1836de560d54cb
UdeshGLeksh <85166833+udeshgn08-dev@users.noreply.github.com>
2021-10-12 19:04:29 +0530

    program00

---
+++ harvard/escapesequence/Overview.java
@@ -0,0 +1,37 @@
+/**
+ * This is the overview of how java programming language looks like 
+ */
+package com.starter;
+
+// Here, import classes if that's necessary for the program development 
+
+public class Overview {
+	/**
+	 * The main method always begins with the same header.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// In java, all programs consist of one of more classes
+		// Think of a class as a container for the program
+		// Code inside the method is enclosed in curly braces 
+		// Annotations easily identify methods   
+		// Statement typically ends with semicolon
+		// Statements are executed sequentially
+		// Identifiers are naming combination of java programs
+		// Identifiers cannot be the same as a keyword
+		// Java is case-sensitive meaning System.out.print is not the same as system.out.Print which could result in confusions,error. 
+		// Literals specified explicitly in the program
+		/* String literal enclosed in double quotes Example: 
+		"Computed Result" and char literals in single quote 'A' */
+		// Constructor name must be the same as Class name
+		// Create an instances of class by creating object for the class.
+		/* { braces are very important part of program flow such as
+		   if,for,while,do-while and switch } */
+		// However, square bracket[] is used to index access elements in arrays,strings
+		/*
+		 * @see oracle doc
+		 */
+	}
+
+} // End of the line
