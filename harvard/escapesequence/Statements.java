f6a2278fd383e8d7fd0e1174a072304a6bfa5eed
UdeshGLeksh <85166833+udeshgn08-dev@users.noreply.github.com>
2021-10-12 19:39:08 +0530

    program02

---
+++ harvard/escapesequence/Statements.java
@@ -0,0 +1,17 @@
+/*
+ * Design statements 
+ */
+package com.starter;
+
+public class Statements {
+	public static void invokeMsg() {
+		System.out.println("\n" + "             " + "I need some Java");
+		System.out.print("\n" + "I need some Java");
+		System.out.print("          I need some Java");
+		System.out.println("\n\n             I need some Java");
+	}
+
+	public static void main(String[] args) {
+		invokeMsg();
+	}
+}
