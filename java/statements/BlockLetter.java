a895085a460ab9624620b4dae76dd88a14419d4b
UdeshGLeksh <85166833+udeshgn08-dev@users.noreply.github.com>
2021-11-14 13:02:11 +0530

    Code 04 > Print letters

---
+++ java/statements/BlockLetter.java
@@ -0,0 +1,19 @@
+/************************************
+* Block letter pattern 
+* Compilation: javac BlockLetter.java
+* Execution: java BlockLetter
+*************************************/
+public class BlockLetter { 
+    
+  public static void main(String[] args) { 
+  
+    System.out.println("  *****  " + " *     * " + " *******"); 
+    System.out.println(" *       " + " *     * " + " *        *"); 
+    System.out.println("******** " + " *     * " + " *         *"); 
+    System.out.println("*      * " + " *     * " + " *         *"); 
+    System.out.println(" ******* " + " *     * " + " *         *"); 
+    System.out.println("       * " + " *     * " + " *        *"); 
+    System.out.println("       * " + "   * *  " + "  *******");
+    
+    }
+}
