6db941e5aee1264398fd88556d8c8daa1ea12df1
UdeshGLeksh <85166833+udeshgn08-dev@users.noreply.github.com>
2021-11-14 12:16:54 +0530

    Code 02 > Escape Sequence

---
+++ java/statements/Ex2EscapeSeq.java
@@ -0,0 +1,16 @@
+/************************************
+* The printf and format methods 
+* Compilation: javac Ex2EscapeSeq.java
+* Execution: java Ex2EscapeSeq
+* Output: 
+* "Input string"
+* '      A' 
+*************************************/ 
+public class Ex2EscapeSeq { 
+
+   public static void main(String[] args) { 
+   
+      System.out.format("\n%n\"Input string\" %n\n\'\tA\'\n%n"); 
+      
+     }
+}
