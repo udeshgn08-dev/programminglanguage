89d36b73a8e7f6995a5fe434dc8eeb41719961fc
UdeshGLeksh <85166833+udeshgn08-dev@users.noreply.github.com>
2021-11-23 17:24:51 +0530

    Add files via upload

---
+++ java/BlockLetter.java
@@ -0,0 +1,21 @@
+/************************************
+* Block letter pattern 
+* 
+* Compilation: javac BlockLetter.java
+* Execution: java BlockLetter
+*************************************/
+package ch01.basics;
+
+public class BlockLetter {	    
+	  public static void main(String[] args) { 
+	  
+	    System.out.println("  *****  " + " *     * " + " *******"); 
+	    System.out.println(" *       " + " *     * " + " *        *"); 
+	    System.out.println("******** " + " *     * " + " *         *"); 
+	    System.out.println("*      * " + " *     * " + " *         *"); 
+	    System.out.println(" ******* " + " *     * " + " *         *"); 
+	    System.out.println("       * " + " *     * " + " *        *"); 
+	    System.out.println("       * " + "   * *  " + "  *******");
+	    
+	    }
+	}
